18c96f67ace63048184ed5c003aad0359d2abb6c
mirzaa <mirzaa@msoe.edu>
2024-09-16 18:47:24 -0500

    Finish Die and DieNotRolledException.

--- src/mirzaa/Die.java
+++ src/mirzaa/Die.java
@@ -2,30 +2,43 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * Die class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Ayaan Mirza
+ * Last Updated: Ayaan Mirza
  */
 package mirzaa;
 
 import java.util.Random;
 
+/**
+ * The Die class. This holds a Die that we can roll and get its current value.
+ */
 public class Die {
     private final int numSides;
     private int currentValue = 0;
-    final int MAX_VALUE = 100;
-    final int MIN_VALUE = 2;
+    private static final int MAX_VALUE = 100;
+    private static final int MIN_VALUE = 2;
     private final Random random = new Random();
     public Die(int sides) {
-        if (sides > MAX_VALUE || sides < MIN_VALUE)
-        {
-            throw new IllegalArgumentException("The number of sides is outside of the range (2-100)");
+        if (sides > MAX_VALUE || sides < MIN_VALUE) {
+            throw new IllegalArgumentException("The number of sides" +
+                    " is outside of the range (2-100)");
         }
         this.numSides = sides;
     }
+
+    /**
+     * This gives us a random integer from 1 to numSides
+     */
     public void roll() {
 //        System.out.println(numSides);
         this.currentValue = random.nextInt(numSides) + 1;
     }
+
+    /**
+     * Returns the current value of the die.
+     * @return the value after the die is rolled
+     * @throws DieNotRolledException if the die was not rolled
+     */
     public int getCurrentValue() {
         if (this.currentValue == 0) {
             System.out.println(this.currentValue);
--- src/mirzaa/Driver.java
+++ src/mirzaa/Driver.java
@@ -2,8 +2,8 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * Main Driver class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Ayaan Mirza
+ * Last Updated: Ayaan Mirza
  */
 package mirzaa;
 
